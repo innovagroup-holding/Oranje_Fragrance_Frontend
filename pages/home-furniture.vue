cb2b4fbc258f794e5c522c11c21ced9cfa55edf8
Moneer AlDakkak <moneeraldakkak@gmail.com>
2024-09-11 07:25:40 +0300

    Initial commit

---
+++ pages/home-furniture.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="home-page-wrapper">
+        <HeaderWithTopbar containerClass="container" />
+        <HeroSliderFurniture />
+        <BannerStyleFive />
+        <ProductWrapperFurniture />
+        <DealWithCountdownTwo />
+        <ServicePolicyTwo />
+        <BlogWrapper />
+        <TheFooter />
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+            HeaderWithTopbar: () => import('@/components/HeaderWithTopbar'),
+            HeroSliderFurniture: () => import('@/components/hero/HeroSliderFurniture'),
+            BannerStyleFive: () => import('@/components/banner/BannerStyleFive'),
+            ProductWrapperFurniture: () => import('@/components/product/ProductWrapperFurniture'),
+            DealWithCountdownTwo: () => import('@/components/DealWithCountdownTwo'),
+            ServicePolicyTwo: () => import('@/components/policy/ServicePolicyTwo'),
+            BlogWrapper: () => import('@/components/BlogWrapper'),
+            TheFooter: () => import('@/components/TheFooter'),
+        },
+        head() {
+            return {
+                title: 'Home Furniture'
+            }
+        },
+    };
+</script>
+
+
